a82e7996f08fdb87db1be804d5fad2642c8e5819
Leandro Araújo <leandro.arauj047.edu@gmail.com>
2021-10-12 00:10:52 -0300

    Adicionando

--- Projeto_07/_js/script.js
+++ Projeto_07/_js/script.js
@@ -1,16 +1,33 @@
 $(function () {
-
   var indiceAtual = 0
   var indiceMaximo = $('.slider img').length
   var delay = 5000
 
   initSlider()
+  cliqueSlider()
 
   function initSlider() {
+    for (var i = 0; i < indiceMaximo; i++) {
+      if (i == 0) {
+        $('.bullets-nav').append('<span style="background-color: red"></span>')
+      } else {
+        $('.bullets-nav').append('<span></span>')
+      }
+    }
     $('.slider img').eq(0).fadeIn()
     setInterval(function () {
       alternarSlider()
     }, delay)
+  }
+
+  function cliqueSlider() {
+    $('.bullets-nav span').click(function () {
+      $('.slider img').eq(indiceAtual).fadeOut(2000)
+      indiceAtual = $(this).index()
+      $('.slider img').eq(indiceAtual).fadeIn(2000)
+      $('.bullets-nav span').css('background-color', '#ccc')
+      $(this).css('background-color', 'red')
+    })
   }
 
   function alternarSlider() {
@@ -20,5 +37,7 @@ $(function () {
       indiceAtual = 0
     }
     $('.slider img').eq(indiceAtual).fadeIn(2000)
+    $('.bullets-nav span').css('background-color', '#ccc')
+    $('.bullets-nav span').eq(indiceAtual).css('background-color', 'red')
   }
 })
